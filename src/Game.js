fd140a1aca28282df9c203bf4e68b71861655945
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-15 22:44:25 +0300

    Update Game component

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,25 @@
-const Game = () => (
-  <div className="game"></div>
-);
+import { Component } from 'inferno';
 
-export default Game;
+import Board from './components/Board';
+import Info from './components/Info';
+
+export default class Game extends Component {
+  state = {
+    scale: 9,
+  }
+
+  render() {
+    const { scale } = this.state;
+
+    return (
+      <div className="game">
+        <div className="game__board">
+          <Board {...{scale}} />
+        </div>
+        <div className="game__info">
+          <Info />
+        </div>
+      </div>
+    );
+  };
+}
