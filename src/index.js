4008333ce1fd3860f1b88afa3a1c4192c8adddb6
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-16 12:54:57 +0300

    Enable the Inferno development tool

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { render } from 'inferno';
 import * as serviceWorker from './serviceWorker';
+import { initDevTools } from 'inferno-devtools';
 
 import App from './Game';
 import './styles/index.css';
@@ -7,3 +8,5 @@ import './styles/index.css';
 render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
+
+initDevTools();
